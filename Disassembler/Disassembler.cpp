d3042143f23d040a9932ad45ca423d3992eaa847
xcb <1226934457@qq.com>
2019-11-11 00:36:26 +0800

    暂时保存

--- Disassembler/Disassembler.cpp
+++ Disassembler/Disassembler.cpp
@@ -1,17 +1,154 @@
 #include <stdio.h>
+#include <Windows.h>
 
-const char* Rb[8] = { "AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH" };
-const char* ERX[8] = { "EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI" };
+const char* r8[8] = { "AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH" };
+const char* r32[8] = { "EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI" };
 const char* JCC[16] = { "JO", "JNO", "JB/JNAE/JC", "JNB/JAE/JNC", "JZ/JE", "JNZ/JNE", "JBE/JNA", "JNBE/JA",
 						"JS", "JNS", "JP/JPE", "JNP/JPO", "JL/JNGE", "JNL/JGE", "JLE/JNG", "JNLE/JG" };
 
+void getBase(char* BaseStr, size_t Mod, size_t Base) {
+	if (Mod == 0 && Base == 5) {
+		sprintf(BaseStr, "disp32");
+	}
+	else {
+		sprintf(BaseStr, "%s", r32[Base]);
+	}
+}
+
+void getScaledIndex(char* ScaledIndex, size_t Scale, size_t Index) {
+	if (Index == 4) {
+		return;
+	}
+
+	switch (Scale)
+	{
+	case 0:
+		sprintf(ScaledIndex, "%s", r32[Index]);
+		break;
+	case 1:
+		sprintf(ScaledIndex, "%s*2", r32[Index]);
+		break;
+	case 2:
+		sprintf(ScaledIndex, "%s*4", r32[Index]);
+		break;
+	case 3:
+		sprintf(ScaledIndex, "%s*8", r32[Index]);
+		break;
+	}
+}
+
+void getSIB(char* sibStr, size_t Mod) {
+	size_t sib;
+	scanf("%x", &sib);
+
+	size_t Scale = sib & 0xC0;
+	size_t Index = sib & 0x38;
+	size_t Base = sib & 0x07;
+
+	char BaseStr[20] = { 0 };
+	char ScaledIndex[20] = { 0 };
+	getBase(BaseStr, Mod, Base);
+	getScaledIndex(ScaledIndex, Scale, Index);
+
+	if (!ScaledIndex) {
+		sprintf(sibStr, "%s", BaseStr);
+	}
+	else {
+		sprintf(sibStr, "%s+%s", BaseStr, ScaledIndex);
+	}
+}
+
+// 获取Gb或Gv
+void getG(char* G, size_t RegOpcode, bool isByte) {
+	if (isByte) {
+		sprintf(G, "%s", r8[RegOpcode]);
+	}
+	else {
+		sprintf(G, "%s", r32[RegOpcode]);
+	}
+}
+
+// 获取Eb或Ev
+void getE(char* E, size_t Mod, size_t RM, bool isByte) {
+	if (Mod == 3) {
+		if (isByte) {
+			sprintf(E, "%s", r8[RM]);
+		}
+		else {
+			sprintf(E, "%s", r32[RM]);
+		}
+		return;
+	}
+
+	char prefix[20] = { 0 };
+	if (isByte) {
+		sprintf(prefix, "BYTE PTR");
+	}
+	else {
+		sprintf(prefix, "DWORD PTR");
+	}
+
+	char addr[50] = { 0 };
+	if (Mod == 0 && RM == 5) {
+		sprintf(addr, "disp32");
+	}
+	else if (RM == 4) {
+		getSIB(addr, Mod);
+	}
+	else {
+		sprintf(addr, "%s", r32[RM]);
+	}
+
+	switch (Mod)
+	{
+	case 0:
+		sprintf(E, "%s DS:[%s]", prefix, addr);
+		break;
+	case 1:
+		sprintf(E, "%s DS:[%s+disp8]", prefix, addr);
+		break;
+	case 2:
+		sprintf(E, "%s DS:[%s+disp32]", prefix, addr);
+		break;
+	}
+}
+
+// 获取EbGb或EvGv或GbEb或GvEv
+void getEG(char* EG, bool isByte, bool isEG) {
+	size_t ModRM;
+	scanf("%x", &ModRM);
+
+	size_t Mod = ModRM & 0xC0;
+	size_t RegOpcode = ModRM & 0x38;
+	size_t RM = ModRM & 0x07;
+
+	char E[50] = { 0 };
+	char G[20] = { 0 };
+	getE(E, Mod, RM, isByte);
+	getG(G, RegOpcode, isByte);
+
+	if (isEG) {
+		sprintf(EG, "%s,%s", E, G);
+	}
+	else {
+		sprintf(EG, "%s,%s", G, E);
+	}
+}
+
 int main() {
-	int a;
+	size_t Opcode;
+	char EG[50] = { 0 };
+
 	while (true) {
-		scanf("%x", &a);
-		switch (a)
+		scanf("%x", &Opcode);
+		switch (Opcode)
 		{
-			// PUSH ERX
+		case 0x00:
+			// ADD Eb, Gb
+			getEG(EG, true, true);
+			printf("ADD %s\n", EG);
+			break;
+
 		case 0x50:
 		case 0x51:
 		case 0x52:
@@ -20,7 +157,8 @@ int main() {
 		case 0x55:
 		case 0x56:
 		case 0x57:
-			printf("PUSH %s\n", ERX[a - 0x50]);
+			// PUSH ERX
+			printf("PUSH %s\n", r32[Opcode - 0x50]);
 			break;
 
 			// POP ERX
@@ -32,7 +170,7 @@ int main() {
 		case 0x5D:
 		case 0x5E:
 		case 0x5F:
-			printf("POP %s\n", ERX[a - 0x58]);
+			printf("POP %s\n", r32[Opcode - 0x58]);
 			break;
 
 			// INC ERX
@@ -44,7 +182,7 @@ int main() {
 		case 0x45:
 		case 0x46:
 		case 0x47:
-			printf("INC %s\n", ERX[a - 0x40]);
+			printf("INC %s\n", r32[Opcode - 0x40]);
 			break;
 
 			// DEC ERX
@@ -56,7 +194,7 @@ int main() {
 		case 0x4D:
 		case 0x4E:
 		case 0x4F:
-			printf("DEC %s\n", ERX[a - 0x48]);
+			printf("DEC %s\n", r32[Opcode - 0x48]);
 			break;
 
 			// MOV Rb, Ib
@@ -68,7 +206,7 @@ int main() {
 		case 0xB5:
 		case 0xB6:
 		case 0xB7:
-			printf("MOV %s, Ib\n", Rb[a - 0xB0]);
+			printf("MOV %s,Ib\n", r8[Opcode - 0xB0]);
 			break;
 
 			// MOV ERX, Id
@@ -80,7 +218,7 @@ int main() {
 		case 0xBD:
 		case 0xBE:
 		case 0xBF:
-			printf("MOV %s, Id\n", ERX[a - 0xB8]);
+			printf("MOV %s,Id\n", r32[Opcode - 0xB8]);
 			break;
 
 			// XCHG EAX, ERX
@@ -94,7 +232,7 @@ int main() {
 		case 0x95:
 		case 0x96:
 		case 0x97:
-			printf("XCHG EAX, %s\n", ERX[a - 0x90]);
+			printf("XCHG EAX,%s\n", r32[Opcode - 0x90]);
 			break;
 
 			// JCC Ib
@@ -114,12 +252,12 @@ int main() {
 		case 0x7D:
 		case 0x7E:
 		case 0x7F:
-			printf("%s Ib\n", JCC[a - 0x70]);
+			printf("%s Ib\n", JCC[Opcode - 0x70]);
 			break;
 
 		case 0x0F:
-			scanf("%x", &a);
-			switch (a)
+			scanf("%x", &Opcode);
+			switch (Opcode)
 			{
 				// JCC Id
 			case 0x80:
@@ -138,7 +276,7 @@ int main() {
 			case 0x8D:
 			case 0x8E:
 			case 0x8F:
-				printf("%s Id\n", JCC[a - 0x80]);
+				printf("%s Id\n", JCC[Opcode - 0x80]);
 				break;
 			}
 
@@ -182,12 +320,12 @@ int main() {
 			break;
 
 
-		case 0xCA:
-			printf("RETF Iw\n");
-			break;
-		case 0xCA:
-			printf("RETF Iw\n");
-			break;
+			/*case 0xCA:
+				printf("RETF Iw\n");
+				break;
+			case 0xCA:
+				printf("RETF Iw\n");
+				break;*/
 		}
 	}
 	return 0;
